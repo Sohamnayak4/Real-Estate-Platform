75b863b2ff685bdebef75b641ad470e864fb16ee
Soham Nayak <sohamnayak04@gmail.com>
2024-05-17 12:08:30 +0530

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Profile from './pages/Profile'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import About from './pages/about'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
